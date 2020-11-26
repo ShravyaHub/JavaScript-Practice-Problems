870b45bf54406d5a7264c6489cfd4dd2cbffad35
ShravyaHub <shravyak710@gmail.com>
2020-11-26 18:14:55 +0530

    ADDED: sequences practice problems

---
+++ Sequence Problems/SumAndAverage.js
@@ -0,0 +1,10 @@
+let number1 = Math.floor(Math.random() * 10) % 90 + 10;
+let number2 = Math.floor(Math.random() * 10) % 90 + 10;
+let number3 = Math.floor(Math.random() * 10) % 90 + 10;
+let number4 = Math.floor(Math.random() * 10) % 90 + 10;
+let number5 = Math.floor(Math.random() * 10) % 90 + 10;
+
+let sum = number1 + number2 + number3 + number4 + number5;
+let average = sum / 5;
+
+console.log("Sum = " + sum + ", Average = " + average);
